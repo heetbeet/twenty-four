fc5610e5bc8538a2a6535cbeb8037c2d09c1fa24
heetbeet <sfstreicher@gmail.com>
2019-11-27 15:15:15 +0200

    slight cleanup

--- twenty-four.py
+++ twenty-four.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python
-
-
-def four_digits_permutations(digits):
-    for i in range(4):
-        for j in range(4):
-            if j not in (i,):
-                for k in range(4):
-                    if k not in (i, j):
-                        for l in range(4):
-                            if l not in (i, j, k):
-                                yield (digits[i], digits[j], digits[k], digits[l])
-
-
-def three_operations_combinations():
-    ops = '+-*/'
-    for i in range(4):
-        for j in range(4):
-            for k in range(4):
-                yield (ops[i], ops[j], ops[k])
-
+import itertools
 
 while 1:
     digits = input("Enter four digits [0-9]: ")
     answers = set()
 
     # Loop through all permutations of 4 digits out of 4:
-    for A, B, C, D in four_digits_permutations(digits):
+    for A, B, C, D in itertools.permutations(digits):
 
         # Loop through all combinations of 3 operations out of 4:
-        for o1, o2, o3 in three_operations_combinations():
+        for x, y, z in itertools.product(*['+-*/']*3):
 
             # There are only two possible BODMAS structures. Test both.
             for cmd in (
-                    f"ans = ({A} {o1} {B}) {o2} ({C} {o3} {D})",
-                    f"ans = (({A} {o1} {B}) {o2} {C}) {o3} {D}"
+                    f"({A} {x} {B}) {y} ({C} {z} {D})",
+                    f"(({A} {x} {B}) {y} {C}) {z} {D}"
             ):
                 try:
-                    # Running exec will set the "ans" variable.
-                    exec(cmd)
+                    # Evaluate "cmd" as Python code.
+                    ans = eval(cmd)
                 except ZeroDivisionError as e:
                     pass
                 else:
                     # noinspection PyUnresolvedReferences
-                    if ans == 24:
-                        if cmd not in answers:
-                            answers.add(cmd)
-                            print(cmd)
+                    if ans == 24:                        
+                        answers.add(cmd)
+                        print(cmd)
 
-    print('Number of answers:', len(answers))
+    print('Number of answers: ≤', len(answers))
     print()
